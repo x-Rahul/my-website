dbe43bedc1a60e5f5b66181c3890ff4e723d0e61
Rahul Meena <rahul1998@iitg.ac.in>
2024-10-09 01:25:25 +0530

    modified

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-document.getElementById('userForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+    document.getElementById('userForm').addEventListener('submit', function(event) {
+        event.preventDefault();
 
-    const formData = {
-        name: document.getElementById('name').value,
-        sport: document.getElementById('sport').value
-    };
+        const formData = {
+            name: document.getElementById('name').value,
+            sport: document.getElementById('sport').value
+        };
 
-    fetch('https://script.google.com/macros/s/AKfycbwiLYGkLxRDUnrPfI2h2-o3nB4YZLOT1mYxa7HNChbP/dev', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-        alert("Data submitted successfully!");
-    })
-    .catch((error) => {
-        console.error('Error:', error);
+        fetch('https://script.google.com/macros/s/AKfycbymWfJjSiXT6LJoeWv6EZV3SEIot6FCOsao03l58I7vbZnhJmzeHBAIl7oiFTri6PUu5w/exec', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            alert("Data submitted successfully!");
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
     });
-});
